84fbeb4e115786e1275baf0bf1edca20670482db
vaibhav goel <vaibhavgoel0230@gmail.com>
2021-05-01 14:30:29 +0530

    first commit

---
+++ urlshort.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template,request,redirect,url_for,flash,abort,session,jsonify
+import json
+import os.path
+app=Flask(__name__)
+app.secret_key='vgvirtual'
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/your-short-codes')
+def short_codes():
+    if os.path.exists('urls.json'):
+        return render_template('shortcodes.html',codes=session.keys())
+    else:
+        session.clear()
+        return render_template('shortcodes.html')
+
+@app.route('/your-url',methods=['GET','POST'])
+def your_url():
+    if request.method=='POST':
+        urls={}
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls=json.load(urls_file)
+        if request.form['code'] in urls.keys():
+            flash('That short name has already been taken.Please use another name.')
+            return redirect(url_for('index'))
+        
+        urls[request.form['code']]={'url':request.form['url']}
+        with open('urls.json','w') as url_file:
+            json.dump(urls,url_file)
+            session[request.form['code']]=True
+        return render_template('your_url.html',code=request.form['code'])
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+            #else:
+            #    flash('No such short name exists. Please enter the correct short name.')
+            #    return redirect(url_for('index'))
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'),404
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
